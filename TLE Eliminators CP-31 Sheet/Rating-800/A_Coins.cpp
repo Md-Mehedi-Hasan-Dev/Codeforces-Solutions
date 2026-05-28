94b6bb2488fbf650621e03f62839665dc3fdeed7
Md. Mehedi Hasan <mehedi85879@gmail.com>
2026-05-28 23:56:52 +0600

    TLE CP-31 800 on EID Day

---
+++ TLE Eliminators CP-31 Sheet/Rating-800/A_Coins.cpp
@@ -0,0 +1,36 @@
+// - Md. Mehedi Hasan
+
+#include <bits/stdc++.h>
+#define Md_Mehedi_Hasan ios_base::sync_with_stdio(false); cin.tie(nullptr); cout.tie(nullptr);
+using namespace std;
+#define nl '\n'
+#define yes cout << "YES" << nl
+#define no cout << "NO" << nl
+#define pb push_back
+#define ff first
+#define ss second
+#define vi vector<int>
+#define vll vector<long long>
+#define ip(x) for (auto &it : x) cin >> it
+#define all(x) x.begin(), x.end()
+using ll = long long;
+using ull = unsigned long long;
+
+void solve()
+{
+    ull n, k; cin >> n >> k;
+    if((n % 2 == 0) || (k & 1 == 1)) yes;
+    else no;
+}
+
+int main()
+{
+    Md_Mehedi_Hasan;
+
+    int t; cin >> t;
+    while(t--) solve();
+
+    return 0;
+}
+
+// https://codeforces.com/problemset/problem/1814/A
---
+++ TLE Eliminators CP-31 Sheet/Rating-800/A_Grasshopper_on_a_Line.cpp
@@ -0,0 +1,44 @@
+// - Md. Mehedi Hasan
+
+#include <bits/stdc++.h>
+#define Md_Mehedi_Hasan ios_base::sync_with_stdio(false); cin.tie(nullptr); cout.tie(nullptr);
+using namespace std;
+#define nl '\n'
+#define yes cout << "YES" << nl
+#define no cout << "NO" << nl
+#define pb push_back
+#define ff first
+#define ss second
+#define vi vector<int>
+#define vll vector<long long>
+#define ip(x) for (auto &it : x) cin >> it
+#define all(x) x.begin(), x.end()
+using ll = long long;
+using ull = unsigned long long;
+
+void solve()
+{
+    int x, k; cin >> x >> k;
+    if(x % k == 0)
+    {
+        cout << 2 << nl;
+        cout << x - 1 << " " << 1 << endl;
+    }
+    else
+    {
+        cout << 1 << nl;
+        cout << x << nl;
+    }
+}
+
+int main()
+{
+    Md_Mehedi_Hasan;
+
+    int t; cin >> t;
+    while(t--) solve();
+
+    return 0;
+}
+
+// https://codeforces.com/problemset/problem/1837/A
