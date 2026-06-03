bfff23908ad58253672fae8d224949b9648e6d40
Md. Mehedi Hasan <mehedi85879@gmail.com>
2026-06-03 23:43:52 +0600

    TLE CP-31 900 Practices

---
+++ TLE Eliminators CP-31 Sheet/Rating-900/B_Longest_Divisors_Interval.cpp
@@ -0,0 +1,43 @@
+// - Md. Mehedi Hasan
+
+#include <bits/stdc++.h>
+#define Md_Mehedi_Hasan ios_base::sync_with_stdio(false); cin.tie(nullptr); cout.tie(nullptr);
+using namespace std;
+#define nl '\n'
+#define yes cout << "YES" << nl
+#define no cout << "NO" << nl
+#define pb push_back
+#define ff first
+#define ss second
+#define vi vector<int>
+#define vll vector<long long>
+#define ip(x) for (auto &it : x) cin >> it
+#define all(x) x.begin(), x.end()
+using ll = long long;
+using ull = unsigned long long;
+
+void solve()
+{
+    ll n; cin >> n;
+    int i, ans = 0;
+    for (int i = 1; i <= 50; i++) 
+    {
+        if (n % i == 0)
+            ans++;
+        else
+            break;
+    }
+    cout << ans << nl;
+}
+
+int main()
+{
+    Md_Mehedi_Hasan;
+
+    int t; cin >> t;
+    while(t--) solve();
+
+    return 0;
+}
+
+// https://codeforces.com/problemset/problem/1855/B
---
+++ TLE Eliminators CP-31 Sheet/Rating-900/D_Balanced_Round.cpp
@@ -0,0 +1,45 @@
+// - Md. Mehedi Hasan
+
+#include <bits/stdc++.h>
+#define Md_Mehedi_Hasan ios_base::sync_with_stdio(false); cin.tie(nullptr); cout.tie(nullptr);
+using namespace std;
+#define nl '\n'
+#define yes cout << "YES" << nl
+#define no cout << "NO" << nl
+#define pb push_back
+#define ff first
+#define ss second
+#define vi vector<int>
+#define vll vector<long long>
+#define ip(x) for (auto &it : x) cin >> it
+#define all(x) x.begin(), x.end()
+#define rall(v) v.rbegin(),v.rend()
+#define sz(a) (int)a.size()
+using ll = long long;
+using ull = unsigned long long;
+
+void solve()
+{
+    ll n, k, ans = 1, cnt = 1; cin >> n >> k;
+    vll v(n);
+    ip(v);
+    sort(all(v));
+    for(int i = 1; i < n; i++)
+    {
+        (abs(v[i] - v[i - 1]) <= k) ? cnt++ : cnt = 1;
+        ans = max(cnt, ans);
+    }
+    cout << n - ans << nl;
+}
+
+int main()
+{
+    Md_Mehedi_Hasan;
+
+    int t; cin >> t;
+    while(t--) solve();
+
+    return 0;
+}
+
+// https://codeforces.com/problemset/problem/1850/D
