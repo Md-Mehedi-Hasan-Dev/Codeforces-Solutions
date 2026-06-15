3c1f4c432596f9ca16d0590c99f35a8e9c60e6a3
Md. Mehedi Hasan <mehedi85879@gmail.com>
2026-06-15 23:09:48 +0600

    IUPC Problem Practice

---
+++ Codeforce Pratice Solutions/B_Peak_Reduction.cpp
@@ -0,0 +1,39 @@
+// - Md. Mehedi Hasan
+
+#include <bits/stdc++.h>
+#define Md_Mehedi_Hasan ios_base::sync_with_stdio(false); cin.tie(nullptr); cout.tie(nullptr);
+using namespace std;
+#define nl '\n'
+#define yes cout << "YES" << nl
+#define no cout << "NO" << nl
+#define pb push_back
+#define ff first
+#define ss second
+#define vi vector<int>
+#define vll vector<long long>
+#define ip(x) for (auto &it : x) cin >> it
+#define all(x) x.begin(), x.end()
+#define rall(v) v.rbegin(),v.rend()
+#define sz(a) (int)a.size()
+using ll = long long;
+using ull = unsigned long long;
+
+void solve()
+{
+    int n; cin >> n;
+    vi v(n);
+    ip(v);
+    ((v[0] == 1 && v[n - 1] == 2) || (v[0] == 2 && v[n - 1] == 1)) ? yes : no;
+}
+
+int main()
+{
+    Md_Mehedi_Hasan;
+
+    int t; cin >> t;
+    while(t--) solve();
+
+    return 0;
+}
+
+// https://codeforces.com/gym/106259/problem/I
---
+++ Codeforce Pratice Solutions/G_Genome_Evolution.cpp
@@ -0,0 +1,39 @@
+// - Md. Mehedi Hasan
+
+#include <bits/stdc++.h>
+#define Md_Mehedi_Hasan ios_base::sync_with_stdio(false); cin.tie(nullptr); cout.tie(nullptr);
+using namespace std;
+#define nl '\n'
+#define yes cout << "YES" << nl
+#define no cout << "NO" << nl
+#define pb push_back
+#define ff first
+#define ss second
+#define vi vector<int>
+#define vll vector<long long>
+#define ip(x) for (auto &it : x) cin >> it
+#define all(x) x.begin(), x.end()
+#define rall(v) v.rbegin(),v.rend()
+#define sz(a) (int)a.size()
+using ll = long long;
+using ull = unsigned long long;
+
+void solve()
+{
+    int a, b, tm; cin >> a >> b >> tm;
+    if(tm == 1) cout << a << nl;
+    else if(tm == 2) cout << b << nl;
+    else cout << (a & b) << nl;
+}
+
+int main()
+{
+    Md_Mehedi_Hasan;
+
+    int t; cin >> t;
+    while(t--) solve();
+
+    return 0;
+}
+
+// https://codeforces.com/gym/106356/problem/G
