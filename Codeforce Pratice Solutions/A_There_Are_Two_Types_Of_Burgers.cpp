bc6340505f2c7b77c060cddd429817d583fa4602
Md. Mehedi Hasan <mehedi85879@gmail.com>
2026-07-14 13:43:38 +0600

    Daily Practice

---
+++ Codeforce Pratice Solutions/A_There_Are_Two_Types_Of_Burgers.cpp
@@ -0,0 +1,48 @@
+// - Md. Mehedi Hasan
+
+#include <bits/stdc++.h>
+#define Md_Mehedi_Hasan ios_base::sync_with_stdio(false); cin.tie(nullptr); cout.tie(nullptr);
+using namespace std;
+#define nl '\n'
+#define sp " "
+#define yes cout << "YES" << nl
+#define no cout << "NO" << nl
+#define pb push_back
+#define ff first
+#define ss second
+#define pii pair<int, int>
+#define pll pair<ll, ll>
+#define vi vector<int>
+#define vll vector<long long>
+#define ip(x) for (auto &it : x) cin >> it
+#define all(x) x.begin(), x.end()
+#define rall(v) v.rbegin(),v.rend()
+#define sz(a) (int)a.size()
+using ll = long long;
+using ull = unsigned long long;
+
+void solve()
+{
+    int b, p, f, h, c, ans = 0; cin >> b >> p >> f >> h >> c;
+    for(int x = 0; x <= p; x++)
+    {
+        if(2 * x > b) continue;
+        int remainingBuns = b - 2 * x;
+        int y = min(f, remainingBuns / 2);
+        int profit = x * h + y * c;
+        ans = max(ans, profit);
+    }
+    cout << ans << nl;
+}
+
+int main()
+{
+    Md_Mehedi_Hasan;
+
+    int t; cin >> t;
+    while(t--) solve();
+
+    return 0;
+}
+
+// https://codeforces.com/contest/1207/problem/A
---
+++ Codeforce Pratice Solutions/H_Standoff.cpp
@@ -0,0 +1,49 @@
+// - Md. Mehedi Hasan
+
+#include <bits/stdc++.h>
+#define Md_Mehedi_Hasan ios_base::sync_with_stdio(false); cin.tie(nullptr); cout.tie(nullptr);
+using namespace std;
+#define nl '\n'
+#define sp " "
+#define yes cout << "YES" << nl
+#define no cout << "NO" << nl
+#define pb push_back
+#define ff first
+#define ss second
+#define pii pair<int, int>
+#define pll pair<ll, ll>
+#define vi vector<int>
+#define vll vector<long long>
+#define ip(x) for (auto &it : x) cin >> it
+#define all(x) x.begin(), x.end()
+#define rall(v) v.rbegin(),v.rend()
+#define sz(a) (int)a.size()
+using ll = long long;
+using ull = unsigned long long;
+
+void solve()
+{
+    string s; cin >> s;
+    bool same = true;
+    for(char c : s)
+    {
+        if(c != s[0])
+        {
+            same = false;
+            break;
+        }
+    }
+    cout << (same ? 0 : 2 * s.length() - 2) << nl;
+}
+
+int main()
+{
+    Md_Mehedi_Hasan;
+
+    int t; cin >> t;
+    while(t--) solve();
+
+    return 0;
+}
+
+// https://codeforces.com/gym/106619/problem/H
